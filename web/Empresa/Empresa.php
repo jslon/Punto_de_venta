b27dd3f69f7fab976891f3945d8587b62e0e5c2c
GreivinP <greivin_p@hotmail.com>
2015-06-04 17:22:19 -0600

    Empresa Lista
    -Con interfaz
    -Pruebas listas

--- web/Empresa/Empresa.php
+++ web/Empresa/Empresa.php
@@ -1,9 +1,9 @@
 <?php
 
 
-if ($_POST['Agregar'])
+if ($_POST['agregar'])
 {
-$conec = pg_connect("host=localhost  dbname=postgres user=postgres password=zattarra ") or die("problemas de coneccion " . pg_last_error());
+$conec = pg_connect("host=localhost  dbname=postgres user=postgres password=zattarra ") or die("problemas de conexion " . pg_last_error());
 
 $query = "INSERT INTO empresa(nombre,cedula_juridica,logo) 
 Values ('$_POST[empresa_nombre]' ,'$_POST[empresa_cedula_juridica]' ,'$_POST[empresa_logo]')";
@@ -12,10 +12,10 @@ pg_query($conec,$query) or die('La consulta fallo: ' . pg_last_error());
 echo "Empresa Agregada";
 header('Location:./empresa.html');
 }
-if ($_POST['Modificar'])
+if ($_POST['modificar'])
 {
 	
-	$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=ECCIpsql2015 ") or die("problemas de coneccion " . pg_last_error());
+	$conec = pg_connect("host=localhost  dbname=postgres user=postgres password=zattarra ") or die("problemas de conexion " . pg_last_error());
 
 $query = $query = "UPDATE empresa SET nombre = '$_POST[empresa_nombre]' , logo = '$_POST[empresa_logo]'
 WHERE cedula_juridica = '$_POST[empresa_cedula_juridica]'";
@@ -25,7 +25,7 @@ echo "Empresa Modificada";
 header('Location:./empresa.html');
 }
 
-if ($_POST['Eliminar'])
+if ($_POST['eliminar'])
 {
 }
 
