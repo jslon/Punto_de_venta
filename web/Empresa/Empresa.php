acc3a5686a795d9698832facd8d38b6cbed52841
GreivinP <greivin_p@hotmail.com>
2015-06-04 17:09:39 -0600

    Empresa interfaz
    Falta pruebas

--- web/Empresa/Empresa.php
+++ web/Empresa/Empresa.php
@@ -1,16 +1,18 @@
 <?php
-if ($_POST['insertar'])
+
+
+if ($_POST['Agregar'])
 {
-$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=ECCIpsql2015 ") or die("problemas de coneccion " . pg_last_error());
+$conec = pg_connect("host=localhost  dbname=postgres user=postgres password=zattarra ") or die("problemas de coneccion " . pg_last_error());
 
 $query = "INSERT INTO empresa(nombre,cedula_juridica,logo) 
 Values ('$_POST[empresa_nombre]' ,'$_POST[empresa_cedula_juridica]' ,'$_POST[empresa_logo]')";
 pg_query($conec,$query) or die('La consulta fallo: ' . pg_last_error());
 
 echo "Empresa Agregada";
-header('Location: http://localhost/Punto_de_Venta/Empresa/Empresa.html');
+header('Location:./empresa.html');
 }
-if ($_POST['modificar'])
+if ($_POST['Modificar'])
 {
 	
 	$conec = pg_connect("host=localhost  dbname=Punto_Venta user=postgres password=ECCIpsql2015 ") or die("problemas de coneccion " . pg_last_error());
@@ -20,9 +22,12 @@ WHERE cedula_juridica = '$_POST[empresa_cedula_juridica]'";
 pg_query($conec,$query) or die('La consulta fallo: ' . pg_last_error());
 
 echo "Empresa Modificada";
-header('Location: http://localhost/Punto_de_Venta/Empresa/Empresa.html');
+header('Location:./empresa.html');
 }
 
+if ($_POST['Eliminar'])
+{
+}
 
 
 ?>
